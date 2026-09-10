48595ab64413b7eda3cbac54a3fc06a8d6adb35a
Karishma Dubey <karishma_dubey@epam.com>
2026-09-10 16:23:59 +0530

    whitelisting frontend origin

--- src/app.js
+++ src/app.js
@@ -12,7 +12,14 @@ const { authRouter } = require('./routes/auth.js');
 const { profileRouter } = require('./routes/profile.js');
 const { requestRouter } = require('./routes/request.js');
 const { userRouter } = require('./routes/user.js');
+const cors = require('cors');
 
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -60,7 +60,7 @@ authRouter.post('/login', async (req, res) => {
       res.cookie('token', token, {
         expires: new Date(Date.now() + 8 * 3600000),
       });
-      res.send('login successful');
+      res.send(user);
     } else {
       throw new Error('invalid credential');
     }
